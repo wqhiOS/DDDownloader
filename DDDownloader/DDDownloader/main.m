99d42c56e013165ed9fe9d5a7276dd5d0d98b677
wuqihan <wuqihan123@163.com>
2019-04-10 15:18:15 +0800

    init

---
+++ DDDownloader/DDDownloader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DDDownloader
+//
+//  Created by wuqh on 2019/4/10.
+//  Copyright © 2019 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
